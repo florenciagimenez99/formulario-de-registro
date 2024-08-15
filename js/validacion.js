93d02e0e01a78a00bc3cc8f31a3c0e8613bbdde9
Florencia Giménez <b3rserker1999@gmail.com>
2024-08-15 09:47:47 -0300

    las contraseñas deben ser iguales

--- js/validacion.js
+++ js/validacion.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Selecciona el botón de registro y los campos de entrada
-    const regBtn = document.getElementById('regBtn');
+   
+    const form = document.getElementById('passwordForm');
     const contraseña = document.getElementById('password1');
     const confirmarContraseña = document.getElementById('password2');
     const alertSuccess = document.getElementById('alert-success');
     const alertDanger = document.getElementById('alert-danger');
 
-    // Funciones para mostrar alertas
+    
     function showAlertSuccess() {
         alertSuccess.classList.add('show');
         alertDanger.classList.remove('show');
@@ -17,14 +17,14 @@ document.addEventListener('DOMContentLoaded', function() {
         alertSuccess.classList.remove('show');
     }
 
-    // Manejador del evento click en el botón de registro
-    regBtn.addEventListener('click', function() {
-        // Comparar los valores de los campos de contraseña
+    
+    form.addEventListener('submit', function(event) {
+        
         if (contraseña.value === confirmarContraseña.value) {
             showAlertSuccess();
-            // Aquí puedes agregar el código para manejar el registro, como una llamada a un servidor.
         } else {
             showAlertError();
+            event.preventDefault(); 
         }
     });
 });
