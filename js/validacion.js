da296e6778b273519b0657a1d82ba2dbddcaf028
Catalina Di Marco <127994728+Catalina-343@users.noreply.github.com>
2024-08-15 10:09:12 -0300

    Actualizacion del boton checkbox

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,15 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+document.getElementById('regBtn').addEventListener('click', function() { 
+    let terminos = document.getElementById('terminos').checked;
+    
+    if (!terminos) {
+        document.getElementById('alert-danger').classList.add('show');
+        document.getElementById('alert-success').classList.remove('show');
+        } else {
+        document.getElementById('alert-danger').classList.remove('show'); 
+        document.getElementById('alert-success').classList.add('show');
+    } 
+ });
